67445e4da7219c876cfa665b078fbe7e426c8323
Glitch (hello-webpage) <none>
2019-11-20 23:06:22 +0000

    👗🍔 Checkpoint ./index.html:5643072/4933 ./script.js:5643072/1486 ./style.css:5643072/1547

--- script.js
+++ script.js
@@ -4,6 +4,15 @@
 // prints "hi" in the browser's dev tools console
 console.log("hi");
 
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
+
+
 var waypoint = new Waypoint({
   element: document.getElementById('article'),
   handler: function(direction) {
