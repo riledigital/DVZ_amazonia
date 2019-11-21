2a079cbb5aa43ba5bfee69a76e235bae3cd44b40
Glitch (hello-webpage) <none>
2019-11-21 22:50:23 +0000

    👰📟 Checkpoint ./script.js:5643072/1

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ console.log("hi");
 
 var mymap = L.map('mapid').setView([34.0522, -118.2437], 13);
 
-
 var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 	subdomains: 'abcd',
