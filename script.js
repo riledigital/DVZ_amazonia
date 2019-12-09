ab0f6edda440d86c6fc8f9731d715de465424643
Glitch (amazonia) <none>
2019-12-09 05:24:16 +0000

    🚆🏂 Checkpoint ./script.js:8790206/3082

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ var mapOptions = {
 var point_home = L.latLng(-5.309766, -58.139648);
 
 // make the map
-var mymap = L.map("mapid", mapOptions).setView(point_home, 4);
+var mymap = L.map("mapid", mapOptions).setView(point_home, 5);
 
 // assuming this changes the base layer theme or whatever
 var CartoDB_Positron = L.tileLayer(
@@ -88,6 +88,79 @@ make_waypoint("#burbank", point_burbank, 0, x => {
 
 // a mix of bostock (https://bost.ocks.org/mike/leaflet/) and this tutorial (https://observablehq.com/@sfu-iat355/introduction-to-leaflet-and-d3)
 
+// drawing borders
+fetch(
+  "https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Fparque-borders.geojson?v=1575833062533"
+)
+  .then(function(response) {
+    // Read data as JSON
+    return response.json();
+  })
+  .then(function(data) {
+    // i wasn't sure how to deal with projection. this is copied over
+    // var projection = d3.geoAlbersUsa();
+
+    // Set up the path-drawing function
+    // var path = d3.geoPath().projection(projection);
+    var transform = d3.geoTransform({ point: projectPoint }),
+      path = d3.geoPath().projection(transform);
+    
+    console.log(data)
+    // actual point drawing, just regular d3 stuff
+    var feature = g
+      .selectAll("path")
+      .attr("class", "border")
+      .data(data.features)
+      .join("path")
+      .attr("stroke", "black")
+      .attr("stroke-width", 0.5)
+
+      // leaflet handles coordinates
+      .attr(
+        "cx",
+        d =>
+          mymap.latLngToLayerPoint([
+            d.geometry.coordinates[1],
+            d.geometry.coordinates[0]
+          ]).x
+      )
+      .attr(
+        "cy",
+        d =>
+          mymap.latLngToLayerPoint([
+            d.geometry.coordinates[1],
+            d.geometry.coordinates[0]
+          ]).y
+      )
+      .attr("r", 1);
+
+    // just for debugging, this is actually cool and shows you the html of each point drawn
+    console.log(feature);
+
+    mymap.on("viewreset", reset);
+    reset();
+
+    // according to bostock this function "repositions the svg to cover the features"...not sure what it means but it works lol
+    // i fear this messes up the zoom...but do we need it?
+    function reset() {
+      var bounds = path.bounds(data),
+        topLeft = bounds[0],
+        bottomRight = bounds[1];
+
+      svg
+        .attr("width", bottomRight[0] - topLeft[0])
+        .attr("height", bottomRight[1] - topLeft[1])
+        .style("left", topLeft[0] + "px")
+        .style("top", topLeft[1] + "px");
+
+      g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+
+      feature.attr("d", path);
+    }
+  });
+
+// this is anti-DRY but i'm trying to rough draft this right now so i copied and pasted the code :'(
+// ideally make this into one function that takes cdn/geojson and path or point parameter so it knows what to draw
 fetch(
   "https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Fgeom_parque.geojson?v=1575832072828"
 )
@@ -103,7 +176,8 @@ fetch(
     // var path = d3.geoPath().projection(projection);
     var transform = d3.geoTransform({ point: projectPoint }),
       path = d3.geoPath().projection(transform);
-
+  
+    console.log(data)
     // actual point drawing, just regular d3 stuff
     var feature = g
       .selectAll("circle")
