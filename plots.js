063386f835e99e0739f82fc3ce9d4c53d02de2de
Glitch (amazonia) <none>
2019-12-09 19:32:25 +0000

    🌾🏮 Checkpoint ./plots.js:8790206/183

--- plots.js
+++ plots.js
@@ -11,18 +11,23 @@
 
 // ;)
 var parque_linegraph = {   
-  $schema: "https://vega.github.io/schema/vega-lite/v4.json",
+  // $schema: "https://vega.github.io/schema/vega-lite/v4.json",
   width: "800",
   height: "300",
   data: {
-    url: "https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Fparque_linegraph.csv?v=1575833062445v"
+    url: "https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Fparque_linegraph.csv?v=1575833062445v",
+    format: {
+      type: "csv"
+    }
   },
+  layer: [{}]
   mark: "line",
   encoding: {
     x: {
       bin: false,
+      timeUnit: "year",
       field: "acq_year",
-      type: "quantitative"
+      type: "temporal"
     },
     y: {
       field: "count",
