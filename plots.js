523a37b1c40ef5266426f5fecc122bdb4738c03c
Glitch (amazonia) <none>
2019-12-09 20:07:43 +0000

    🎩🏌 Checkpoint ./plots.js:8790206/3609 ./index.html:8790206/3579

--- plots.js
+++ plots.js
@@ -10,81 +10,87 @@
 // EG: parque_linegraph
 
 // ;)
-var parque_linegraph = {   
-  // $schema: "https://vega.github.io/schema/vega-lite/v4.json",
-  width: "800",
-  height: "300",
-  data: {
-    url: "https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Fparque_linegraph.csv?v=1575833062445v",
-    format: {
-      type: "csv"
-    }
-  },
-  layer: [
-    {
-      encoding: {
-        x: {field: "acq_year", type: "temporal"},
-        y: {field: "count", type: "quantitative"}
-      },
-      layer: [
-        {mark: "line"},
-        {
-          selection: {
-            label: {
-              type: "single",
-              nearest: true,
-              on: "mouseover",
-              encodings: ["x"],
-              empty: "none"
-            }
-          },
-          mark: "point",
-          encoding: {
-            opacity: {
-              condition: {selection: "label", value: 1},
-              value: 0
-            }
-          }
-        }
-      ]
+var linegraph = function (data) {   
+  return {
+    width: "1000",
+    height: "300",
+    data: {
+      url: data,
+      format: {
+        type: "csv"
+      }
     },
-    {
-      transform: [{filter: {selection: "label"}}],
-      layer: [
-        {
-          mark: {type: "rule", color: "gray"},
-          encoding: {
-            x: {type: "temporal", field: "acq_year"}
-          }
+    layer: [
+      {
+        encoding: {
+          x: {field: "acq_year", type: "temporal", axis: {title: "Acquisition year"}},
+          y: {field: "count", type: "quantitative", axis: {title: "Total fire count"}}
         },
-        {
-          encoding: {
-            text: {type: "quantitative", field: "count"},
-            "x": {"type": "temporal", field: "acq_year"},
-            "y": {"type": "quantitative", field: "count"}
-          },
-          "layer": [
-            {
-              "mark": {
-                "type": "text",
-                "stroke": "white",
-                "strokeWidth": 2,
-                "align": "left",
-                "dx": 5,
-                "dy": -5
+        layer: [
+          {mark: "line"},
+          {
+            selection: {
+              label: {
+                type: "single",
+                nearest: true,
+                on: "mouseover",
+                encodings: ["x"],
+                empty: "none"
               }
             },
-            {
-              "mark": {"type": "text", "align": "left", "dx": 5, "dy": -5},
-              "encoding": {
-                "color": {"type": "nominal", "field": "symbol", "legend": null}
+            mark: "point",
+            encoding: {
+              opacity: {
+                condition: {selection: "label", value: 1},
+                value: 0
               }
             }
-          ]
-        }
-      ]
-    }
-  ]
+          }
+        ]
+      },
+      {
+        transform: [{filter: {selection: "label"}}],
+        layer: [
+          {
+            mark: {type: "rule", color: "gray"},
+            encoding: {
+              x: {type: "temporal", field: "acq_year"}
+            }
+          },
+          {
+            encoding: {
+              text: {type: "quantitative", field: "count"},
+              x: {type: "temporal", field: "acq_year"},
+              y: {type: "quantitative", field: "count"}
+            },
+            layer: [
+              {
+                mark: {
+                  type: "text",
+                  stroke: "white",
+                  strokeWidth: 2,
+                  align: "left",
+                  dx: 5,
+                  dy: -5
+                }
+              },
+              {
+                mark: {type: "text", align: "left", dx: 5, dy: -5},
+                encoding: {
+                  color: {type: "nominal", field: "symbol", legend: null}
+                }
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  };
 };
 
+var parque_linegraph = linegraph("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Fparque_linegraph.csv?v=1575833062445");
+var ara_linegraph = linegraph("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Fara_linegraph.csv?v=1575833062680");
+var marai_linegraph = linegraph("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2FMaraiwatsede_linegraph.csv?v=1575833062211");
 vegaEmbed("#parque_linegraph", parque_linegraph);
+vegaEmbed("#ara_linegraph", ara_linegraph);
+vegaEmbed("#marai_linegraph", marai_linegraph);
