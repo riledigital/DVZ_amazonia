38a6830396ca94d9c1978e0fdb33ed6ade42d87d
Glitch (amazonia) <none>
2019-12-10 05:46:40 +0000

    👣👨‍❤️‍💋‍👨 Checkpoint ./leaflet_maps.js:8790206/1272 ./plots.js:8790206/1027

--- leaflet_maps.js
+++ leaflet_maps.js
@@ -30,9 +30,9 @@ addGeoJSONToMap("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Fm
 addGeoJSONToMap("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Faraboia_borders.geojson?v=1575833062709");
 
 const firePoints = {
-  radius: 3,
-  fillColor: red,
-  color: red,
+  radius: 2,
+  fillColor: "#f00",
+  color: "#f00",
   weight: 0,
   opacity: 1,
   fillOpacity: .55,
@@ -47,17 +47,20 @@ const addPointsLayer = (url) => {
       return response.json();
     })
     .then(function(data) {
+      var sorted = data.features.sort((a, b) => a.properties.ACQ_DATE < b.properties.ACQ_DATE ? -1 : ((a.properties.ACQ_DATE > b.properties.ACQ_DATE) ? 1 : 0));
       // Add data to the map
       var myLayer = L.geoJSON(data, {
         pointToLayer: function(feature, latlng) {
           return L.circleMarker(latlng, firePoints);
         }
-      }).addTo(myMap);
+      }).addTo(mymap);
       console.log(data);
     });
 };
 
-addPointsLayer
+addPointsLayer("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Fparque_geom.geojson?v=1575833062519");
+addPointsLayer("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Fara_geom.geojson?v=1575833062609");
+addPointsLayer("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2FMaraiwatsede_geom.geojson?v=1575833062821");
 
 // README Article on using assets lib
 // https://glitch.com/~assets-lib
