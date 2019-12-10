1c2e734339b81cebecd9d3d63d3bc7961a0e3b31
Glitch (amazonia) <none>
2019-12-10 15:34:16 +0000

    🚕📺 Checkpoint ./focus_regions_linegraph.csv:8790206/2260 ./.glitch-assets:8790206/387 ./plots.js:8790206/3885 ./index.html:8790206/42 ./leaflet_maps.js:8790206/18

--- leaflet_maps.js
+++ leaflet_maps.js
@@ -25,9 +25,9 @@ addGeoJSONToMap = (url) => {
   }
 };
 
-addGeoJSONToMap("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Fparque-borders.geojson?v=1575833062533");
-addGeoJSONToMap("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Fmaraiwatsede-borders.geojson?v=1575833062234");
-addGeoJSONToMap("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Faraboia_borders.geojson?v=1575833062709");
+// addGeoJSONToMap("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Fparque-borders.geojson?v=1575833062533");
+// addGeoJSONToMap("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Fmaraiwatsede-borders.geojson?v=1575833062234");
+// addGeoJSONToMap("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Faraboia_borders.geojson?v=1575833062709");
 
 const firePoints = {
   radius: 1,
@@ -72,9 +72,9 @@ const addPointsLayer = (url) => {
 //     });
 })};
 
-addPointsLayer("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Fparque_geom.geojson?v=1575833062519");
-addPointsLayer("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Fara_geom.geojson?v=1575833062609");
-addPointsLayer("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2FMaraiwatsede_geom.geojson?v=1575833062821");
+// addPointsLayer("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Fparque_geom.geojson?v=1575833062519");
+// addPointsLayer("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Fara_geom.geojson?v=1575833062609");
+// addPointsLayer("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2FMaraiwatsede_geom.geojson?v=1575833062821");
 
 //Create sliderControl
 // var sliderControl = L.control.sliderControl({
--- plots.js
+++ plots.js
@@ -87,6 +87,96 @@ var linegraph = function(data) {
               {
                 mark: { type: "text", fontSize: 18, align: "right", dx: -8, dy: 2 },
                 encoding: {
+                  color: { type: "nominal", field: "groupname", legend: null }
+                }
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  };
+};
+
+
+var multi_linegraph = function(data) {
+  return {
+    width: 960,
+    height: 540,
+    data: {
+      url: data,
+      format: {
+        type: "csv"
+      }
+    },
+    layer: [
+      {
+        
+        encoding: {
+          x: {
+            field: "acq_year",
+            type: "temporal",
+            axis: { title: "Acquisition year" }
+          },
+          y: {
+            field: "count",
+            type: "quantitative",
+            axis: { title: "Total fire count" }
+          },
+          "color": {"field": "groupname", "type": "nominal"}
+        },
+        layer: [
+          { mark: "line" },
+          {
+            selection: {
+              label: {
+                type: "single",
+                nearest: true,
+                on: "mouseover",
+                encodings: ["x"],
+                empty: "none"
+              }
+            },
+            mark: "point",
+            encoding: {
+              opacity: {
+                condition: { selection: "label", value: 1 },
+                value: 0
+              }
+            }
+          }
+        ]
+      },
+      {
+        transform: [{ filter: { selection: "label" } }],
+        layer: [
+          {
+            mark: { type: "rule", color: "gray" },
+            encoding: {
+              x: { type: "temporal", field: "acq_year" }
+            }
+          },
+          {
+            encoding: {
+              text: { type: "quantitative", field: "count" },
+              x: { type: "temporal", field: "acq_year" },
+              y: { type: "quantitative", field: "count" }
+            },
+            layer: [
+              {
+                mark: {
+                  type: "text",
+                  fontSize: 18,
+                  stroke: "white",
+                  strokeWidth: 3,
+                  align: "right",
+                  dx: -8,
+                  dy: 2
+                }
+              },
+              {
+                mark: { type: "text", fontSize: 18, align: "right", dx: -8, dy: 2 },
+                encoding: {
                   color: { type: "nominal", field: "symbol", legend: null }
                 }
               }
@@ -98,6 +188,10 @@ var linegraph = function(data) {
   };
 };
 
+var focus_regions_linegraph = multi_linegraph(
+  "https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Ffocus_regions_linegraph.csv?v=1575991690328"
+);
+
 var parque_linegraph = linegraph(
   "https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Fparque_linegraph.csv?v=1575833062445"
 );
@@ -108,6 +202,7 @@ var marai_linegraph = linegraph(
   "https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2FMaraiwatsede_linegraph.csv?v=1575833062211"
 );
 
+vegaEmbed("#focus_regions_linegraph", focus_regions_linegraph);
 vegaEmbed("#parque_linegraph", parque_linegraph);
 vegaEmbed("#ara_linegraph", ara_linegraph);
 vegaEmbed("#marai_linegraph", marai_linegraph);
