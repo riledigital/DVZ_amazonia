6078fb896e72ac3103c8508b94e220bb8ee6c77d
Glitch (amazonia) <none>
2019-12-10 16:44:54 +0000

    🐺🙈 Checkpoint ./script.js:8790206/9

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ var mymap = L.map("mapid", mapOptions).setView(point_home, 7.3);
 
 // assuming this changes the base layer theme or whatever
 var CartoDB_Positron = L.tileLayer(
-  "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+  "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
   {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
