2b873b3bc2ff4d3e948ea510980ddac9781125bf
Glitch (amazonia) <none>
2019-12-10 22:37:27 +0000

    ✍️🚒 Checkpoint ./leaflet_maps.js:8790206/117 ./index.html:8790202/1558

--- leaflet_maps.js
+++ leaflet_maps.js
@@ -74,27 +74,27 @@ const addPointsLayer = (url) => {
           // console.log(feature);
           return L.circleMarker(latlng, firePoints);
         }
-      }).addTo(layerGroupByDate);
+      }).addTo(mymap);
 })};
 
 // addPointsLayer("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Fparque_geom.geojson?v=1575833062519");
 // addPointsLayer("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Fara_geom.geojson?v=1575833062609");
 addPointsLayer("https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2FMaraiwatsede_geom.geojson?v=1575833062821");
 
-var sliderControl = L.control.sliderControl({
-  position: "center",
-  layer: layerGroupByDate,
-  range: true,
-  timeAttribute: "ACQ_DATE",
-  follow: true,
-});
+// console.log(layerGroupByDate);
+// var sliderControl = L.control.sliderControl({
+//   position: "center",
+//   layer: layerGroupByDate,
+//   range: true,
+//   timeAttribute: "ACQ_DATE",
+//   follow: true,
+// });
 
-console.log(maraiPoints);
-//add sliderControl to the map
-mymap.addControl(sliderControl);
+// //add sliderControl to the map
+// mymap.addControl(sliderControl);
 
-initialize sliderControl
-sliderControl.startSlider();
+// // initialize sliderControl
+// sliderControl.startSlider();
 
 // README Article on using assets lib
 // https://glitch.com/~assets-lib
