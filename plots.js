ab3ae18a5c237834d2f7f11422af2bde2c9a4692
Glitch (amazonia) <none>
2019-12-10 05:12:27 +0000

    🐮👁 Checkpoint ./plots.js:8790206/6319

--- plots.js
+++ plots.js
@@ -76,15 +76,16 @@ var linegraph = function(data) {
               {
                 mark: {
                   type: "text",
+                  fontSize: 18,
                   stroke: "white",
-                  strokeWidth: 2,
+                  strokeWidth: 3,
                   align: "right",
                   dx: -8,
-                  dy: 0
+                  dy: 2
                 }
               },
               {
-                mark: { type: "text", align: "right", dx: -8, dy: 0 },
+                mark: { type: "text", fontSize: 18, align: "right", dx: -8, dy: 2 },
                 encoding: {
                   color: { type: "nominal", field: "symbol", legend: null }
                 }
@@ -111,39 +112,41 @@ vegaEmbed("#parque_linegraph", parque_linegraph);
 vegaEmbed("#ara_linegraph", ara_linegraph);
 vegaEmbed("#marai_linegraph", marai_linegraph);
 
-// var summary_streamgraph = {
-//   // "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
-//   "width": 300, "height": 200,
-//     data: {
-//       url: "https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Foverview_streamgraph_fires_by_year.csv?v=1575833062342",
-//       format: {
-//         type: "csv"
-//       }
-//     },
-//   "mark": "area",
-//   "selection": {
-//     "Name": {
-//       "type": "multi", "fields": ["groupname"], "bind": "legend"
-//     }
-//   },
-//   "encoding": {
-//     "x": {
-//       "timeUnit": "year", "field": "acq_year", "type": "temporal",
-//       "axis": {"domain": false, "format": "%Y", "tickSize": 0}
-//     },
-//     "y": {
-//       "aggregate": "sum", "field": "count", "type": "quantitative",
-//       "stack": "center", "axis": null
-//     },
-//     "color": {
-//       "field":"groupname", "type": "nominal",
-//       "scale": {"scheme": "category20b"}
-//     },
-//     "opacity": {
-//       "condition": {"selection": "Name", "value": 1},
-//       "value": 0.2
-//     }
-//   }
-// }
+var summary_streamgraph = {
+  // "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+  "width": 300, "height": 200,
+    data: {
+      url: "https://cdn.glitch.com/e0876ad4-2883-4d2f-bf08-a90e9d4b0b1e%2Foverview_streamgraph_fires_by_year.csv?v=1575833062342",
+      format: {
+        type: "csv"
+      }
+    },
+  "mark": "area",
+  "selection": {
+    "Name": {
+      "type": "multi", "fields": ["groupname"], "bind": "legend"
+    }
+  },
+  "encoding": {
+    "x": {
+      "timeUnit": "year", "field": "acq_year", "type": "temporal",
+      "axis": {"domain": false, "format": "%Y", "tickSize": 0}
+    },
+    "y": {
+      "aggregate": "sum", "field": "count", "type": "quantitative",
+      "stack": "center", "axis": null
+    },
+    "color": {
+      "field":"groupname", "type": "nominal",
+      "scale": {"scheme": "category20b"}
+    },
+    "opacity": {
+      "condition": {"selection": "Name", "value": 1},
+      "value": 0.2
+    }
+  }
+}
 
-// vegaEmbed("#summary_streamgraph", summary_streamgraph)
+
+
+vegaEmbed("#summary_streamgraph", summary_streamgraph)
